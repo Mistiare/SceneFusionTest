af7f6d54453598bedd922e5a55efdc413469f61f
Callum Metcalfe <callummgamedev@hotmail.com>
2021-01-27 22:07:23 +0000

    - makeamanmove.cs

---
+++ SceneFusionTestv2/Assets/Scripts/makeamanmove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class makeamanmove : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5f;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float xDir = Input.GetAxis("Horizontal");
+        float zDir = Input.GetAxis("Vertical");
+        Vector3 targetVelocity = new Vector3(xDir, 0, zDir);
+
+        rb.velocity = targetVelocity * moveSpeed;
+    }
+}
+
+
